0129164622d6a1c5d637f448e85d914dc50f6e4e
coderut <15391493308@163.com>
2023-04-11 21:11:49 +0800

    修改

--- micro-geth/src/main/java/top/xpit/geth/controller/app/AppOrderController.java
+++ micro-geth/src/main/java/top/xpit/geth/controller/app/AppOrderController.java
@@ -1,11 +1,13 @@
 package top.xpit.geth.controller.app;
 
+import com.github.pagehelper.PageHelper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.xpit.common.core.controller.BaseController;
 import top.xpit.common.core.page.TableDataInfo;
+import top.xpit.common.utils.SecurityUtils;
 import top.xpit.geth.domain.MicroBid;
 import top.xpit.geth.domain.MicroOrder;
 import top.xpit.geth.domain.query.AppOrderParam;
@@ -24,6 +26,7 @@ public class AppOrderController extends BaseController {
 
     @GetMapping("list")
     public TableDataInfo list(AppOrderParam param){
+        param.setUserId(SecurityUtils.getAppUserId());
         startPage();
         List<OrderVo> list = microOrderService.selectAppOrderList(param);
         return getDataTable(list);
--- micro-geth/src/main/java/top/xpit/geth/domain/vo/OrderVo.java
+++ micro-geth/src/main/java/top/xpit/geth/domain/vo/OrderVo.java
@@ -1,9 +1,13 @@
 package top.xpit.geth.domain.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import lombok.ToString;
+import top.xpit.common.core.domain.BaseEntity;
 
 import java.util.Date;
 
+@ToString
 @Data
 public class OrderVo {
 
@@ -15,7 +19,7 @@ public class OrderVo {
     /**
      * 标题
      */
-    private String title;
+    private String name;
 
     /**
      * 商品分类
@@ -28,6 +32,7 @@ public class OrderVo {
     /**
      * 订单生成时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss", timezone = "GMT+8")
     private Date orderTime;
     /**
      * 订单状态
