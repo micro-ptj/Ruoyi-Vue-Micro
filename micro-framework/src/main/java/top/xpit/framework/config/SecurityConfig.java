fa814f9f259136a5f92d0474a902ed66472519f7
coderut <15391493308@163.com>
2023-04-19 19:54:47 +0800

    放通验证码

--- micro-framework/src/main/java/top/xpit/framework/config/SecurityConfig.java
+++ micro-framework/src/main/java/top/xpit/framework/config/SecurityConfig.java
@@ -115,7 +115,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 // 静态资源，可匿名访问
                 .antMatchers(HttpMethod.GET, "/", "/*.html", "/**/*.html", "/**/*.css", "/**/*.js", "/profile/**").permitAll()
                 .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/*/api-docs", "/druid/**").permitAll()
-                .antMatchers("/app/login").permitAll()
+                .antMatchers("/app/login", "/app/code").permitAll()
                 .antMatchers("/system/dict/data/type/*").permitAll()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated()
