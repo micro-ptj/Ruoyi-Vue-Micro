471e85da9b479185680e420c3fb895d79e83e81e
ut <67055263+PTJ12@users.noreply.github.com>
2023-05-01 17:15:07 +0800

    放通文件上传接口

--- micro-framework/src/main/java/top/xpit/framework/config/SecurityConfig.java
+++ micro-framework/src/main/java/top/xpit/framework/config/SecurityConfig.java
@@ -117,6 +117,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/*/api-docs", "/druid/**").permitAll()
                 .antMatchers("/app/login", "/app/code").permitAll()
                 .antMatchers("/system/dict/data/type/*").permitAll()
+                .antMatchers("/common/upload").permitAll()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated()
                 .and()
